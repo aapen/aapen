ee0923ac7e52c06a892ae8c21c32802d095b3629
Michael Nygard <mtnygard@gmail.com>
2023-06-08 18:29:48 -0500

    Now gets far enough to trap on MMIO write.
    CPU core check was borked.
    BSS setup was borked.
    Stack setup might be borked... can't tell until we use the stack.

--- boot.s
+++ boot.s
@@ -3,41 +3,50 @@
 
         .section ".text.boot"
 
-        .global _start
+CONST_CORE_ID_MASK = 0b11
+BOOT_CORE_ID       =    0
 
 _start:
         // Check we are on the main core
         //
         // References:
         // https://developer.arm.com/documentation/ddi0500/j/System-Control/AArch64-register-descriptions/Multiprocessor-Affinity-Register?lang=en
-        mrs x1, mpidr_el1
-        and x1, x1, #3
-        cbz x1, 2f
+        mrs x0, MPIDR_EL1
+        and x0, x0, #CONST_CORE_ID_MASK
+        ldr x1, =BOOT_CORE_ID
+        cmp x0, x1
+        b.ne .L_parking_loop
 
-1:
-        // Not on main core, wait forever
-        wfe
-        b 1b
-
-2:
         // On main core
 
+        // Initialize BSS section to zero
+        ldr x0, =__bss_start
+        ldr w1, =__bss_end
+
+.L_bss_init_loop:
+        cmp x0, x1
+        b.eq .L_initialize_stack
+        stp xzr, xzr, [x0], #16
+        b .L_bss_init_loop
+
+.L_initialize_stack:
         // Initialize stack
-        ldr x1, _start
+        // (for now) the stack starts at _start and grows downward.
+        // this is limiting... once we're in kernel space we will
+        // reset this
+        ldr x1, =_start
         mov sp, x1
 
-        // Clean BSS section
-        ldr x1, __bss_start
-        ldr w2, __bss_size
-3:
-        cbz w2, 4f
-        str xzr, [x1], #8
-        sub w2, w2, #1
-        cbnz w2, 3b
-
-4:
         // Jump to main()
-        bl main
+        b main
 
-        // In case main() returns, spinloop
-        b 1b
+        // if main returns, fall through to park the core
+
+        // Park the core
+.L_parking_loop:
+        wfe
+        b .L_parking_loop
+
+        .size _start, . - _start
+        .type _start, function
+        .global _start
