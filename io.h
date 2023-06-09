5c571ea7a554de0d942f78924b72938d29ec2ff4
Michael Nygard <mtnygard@gmail.com>
2023-06-08 19:47:00 -0500

    Switch to using PL011 UART instead of the mini-UART
    This makes the QEMU serial connection work.
    
    Hello world in the emulator achieved.

--- io.h
+++ io.h
@@ -1,7 +1,7 @@
 #ifndef __IO_H
 #define __IO_H
 
-extern void uart_init();
-extern void uart_writeText(char*);
+extern void pl011_uart_init();
+extern void pl011_uart_writeText(char*);
 
 #endif
--- kernel.c
+++ kernel.c
@@ -1,8 +1,8 @@
 #include "io.h"
 
 void main() {
-  uart_init();
-  uart_writeText("Hello, world!\n");
+  pl011_uart_init();
+  pl011_uart_writeText("Hello, world!\n");
 
   while (1)
     ;
