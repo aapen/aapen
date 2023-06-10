7ff727d5e52695b755f757ac951829340f317a54
Michael Nygard <mtnygard@gmail.com>
2023-06-10 18:06:18 -0500

    Zig build for cross-compilation.
    Includes compiling the zig code, assembling the .s files, and linking
    using the custom linker script.
    
    It even makes the binary kernel8.img file we need.
    
    The only thing I can't figure out is how to get it to copy the output
    from under the zig-cache directories to the project root.

---
+++ build.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const Target = std.Target;
+const CrossTarget = std.zig.CrossTarget;
+
+pub fn build(b: *std.Build) void {
+    const target = std.zig.CrossTarget{
+        .cpu_arch = Target.Cpu.Arch.aarch64,
+        .os_tag = Target.Os.Tag.freestanding,
+        .abi = Target.Abi.none,
+    };
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "kernel8.elf",
+        .target = target,
+        .root_source_file = .{ .path = "io.zig" },
+        .optimize = optimize,
+        .link_libc = false,
+    });
+    exe.addAssemblyFile("boot.s");
+    exe.addAssemblyFile("qemu.s");
+    exe.setLinkerScriptPath(.{ .path = "kernel.ld" });
+
+    const objcopy = b.addObjCopy(exe.getOutputSource(), .{
+        .basename = "./kernel8.img",
+        .format = std.Build.Step.ObjCopy.RawFormat.bin,
+    });
+
+    objcopy.step.dependOn(&exe.step);
+    b.default_step.dependOn(&objcopy.step);
+}
