fe8e0858d6f5e15fada2d3c8525b07f7b1211b42
Michael Nygard <mtnygard@gmail.com>
2023-06-13 07:35:21 -0500

    Comment the heck out of the boot code

--- src/boot.s
+++ src/boot.s
@@ -1,55 +1,93 @@
-        // boot code for Cortex-A53 and Cortex-A72
-        // Applies to 3B, 3B+, 4B, 400, CM4
+// Boot code for Cortex-A53 and Cortex-A72
+//
+// Requires a 64-bit architecture
+//
+// This applies to Raspberry Pi 3B, 3B+, 4B, 400, CM4
+
+// ----------------------------------------------------------------------
+// Constants
+// ----------------------------------------------------------------------
+
+CONST_CURRENTEL_EL2 = 0x08
+CONST_CORE_ID_MASK  = 0xff
+BOOT_CORE_ID        = 0x00
+
+// ----------------------------------------------------------------------
+// Macros
+// ----------------------------------------------------------------------
+
+// Load address of a symbol in a register.
+//
+// The symbol can be at most +/- 4 GiB of the PC
+.macro LDR_REL register, symbol
+        adrp \register, \symbol
+        add  \register, \register, #:lo12:\symbol
+.endm
+
+// ----------------------------------------------------------------------
+// fn _start()
+//
+// Arguments: none
+// Returns: none
+// Clobbers: x0, x1
+// ----------------------------------------------------------------------
 
         .section ".text.boot"
 
-CONST_CORE_ID_MASK = 0b11
-BOOT_CORE_ID       =    0
-
-        .globl _start
-        .type  _start,@function
+        .global _start
+        .type   _start, @function
 _start:
-        // Check we are on the main core
+        // At boot time, the core should be executing in EL2 (low
+        // privilege). If it is not, we are in a strange state and
+        // should not proceed
+
+        mrs x0, CurrentEL                     // Get execution level from CPU into x0
+        cmp x0, #CONST_CURRENTEL_EL2          // It should be EL2 at boot time
+        b.ne L_parking_loop                   // If not, something is wrong, park the core
+
+        // All cores boot from the same image. We're going to have one core
+        // handle system initialization. It will later activate the other cores.
+
+        mrs x0, MPIDR_EL1                     // Get the core ID from the CPU's multiprocessor affinity register
+        and x0, x0, #CONST_CORE_ID_MASK       // The lower 8 bits hold the core number
+        ldr x1, =BOOT_CORE_ID                 // The boot core is core 0
+        cmp x0, x1                            // Are we on the boot core?
+        b.ne L_parking_loop                   // If not, park the core
+
+        // We're on the main core. Initialize memory and stack.
         //
-        // References:
-        // https://developer.arm.com/documentation/ddi0500/j/System-Control/AArch64-register-descriptions/Multiprocessor-Affinity-Register?lang=en
-        mrs x0, MPIDR_EL1
-        and x0, x0, #CONST_CORE_ID_MASK
-        ldr x1, =BOOT_CORE_ID
-        cmp x0, x1
-        b.ne .L_parking_loop
+        // The BSS symbols are provided by the linker script, which computes
+        // them from the object files produced by the compiler.
+        //
+        // BSS is never loaded from the program image// only the addresses
+        // are specified. We're required to zero out that range of memory.
 
-        // On main core
+        LDR_REL x0, __bss_start               // Put the starting address in x0
+        LDR_REL x1, __bss_end_exclusive       // Put the last addres + 1 in x1
 
-        // Initialize BSS section to zero
-        ldr x0, =__bss_start
-        ldr w1, =__bss_end_exclusive
+L_bss_init_loop:
+        cmp x0, x1                            // Has x0 reached __bss_end_exclusive?
+        b.eq L_initialize_stack               // If so, we're done
+        stp xzr, xzr, [x0], #16               // Otherwise, store 64 bits of zeros and
+                                              // post-increment x0
+        b L_bss_init_loop                     // Repeat until done
 
-.L_bss_init_loop:
-        cmp x0, x1
-        b.eq .L_initialize_stack
-        stp xzr, xzr, [x0], #16
-        b .L_bss_init_loop
+        // Set up the stack.
+        //
+        // __boot_core_stack_end_exclusive is provided by the linker
+        // script which puts it right below _start itself
 
-.L_initialize_stack:
-        // Initialize stack
-        adrp x0, __boot_core_stack_end_exclusive
-        add  x0, x0, #:lo12:__boot_core_stack_end_exclusive
+L_initialize_stack:
+        LDR_REL x0, __boot_core_stack_end_exclusive
         mov sp, x0
 
-        // Jump to main()
-        bl kernel_main
+        bl kernel_main                        // Jump to function provided by main.zig
 
-        // TEMP: tell QEMU to quit
-        // Use the return value from kernel_main as the exit code
-        mov x2,x0
-        bl _qemu_exit
+        bl _qemu_exit                         // TEMP: Tell qemu to quit
 
         // Park the core
-.L_parking_loop:
-        wfe
-        b .L_parking_loop
+L_parking_loop:
+        wfe                                   // wait for an event
+        b L_parking_loop                      // if one arrives, loop and wait some more
 
-        .size _start, . - _start
-        .type _start, function
-        .global _start
+        .size _start, . - _start              // Tell the assembler how big this symbol is
