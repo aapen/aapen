0eb9b3844602c5e2c0f57c12d9e0d249809a259d
Michael Nygard <mtnygard@gmail.com>
2023-06-18 10:38:54 -0500

    Populating more register definitions.
    SCTLR_EL2 differs from SCTLR_EL1 in just a few places.

--- lib/arch/aarch64/registers.zig
+++ lib/arch/aarch64/registers.zig
@@ -1,13 +1,11 @@
-pub const cntp_ctl = @import("registers/cntp_ctl.zig");
 pub const cnthctl_el2 = @import("registers/cnthctl_el2.zig");
+pub const cntp_ctl = @import("registers/cntp_ctl.zig");
+pub const cntv_ctl = @import("registers/cntv_ctl.zig");
 pub const hcr_el2 = @import("registers/hcr_el2.zig");
-pub const sctlr_el1 = @import("registers/sctlr_el1.zig");
+pub const sctlr = @import("registers/sctlr.zig");
 pub const spsr = @import("registers/spsr.zig");
 
 const R = @import("system_register.zig").UniformSystemRegister;
-//
-// Registers with less detailed explanations are here
-//
 
 // ----------------------------------------------------------------------
 // EL2 (Hypervisor)
@@ -25,6 +23,9 @@ pub const ELR_EL2 = R("ELR_EL2", u64);
 /// Hypervisor Control Register
 pub const HCR_EL2 = R("HCR_EL2", hcr_el2.layout);
 
+/// System Control Register (EL2)
+pub const SCTLR_EL2 = R("SCTLR_EL2", sctlr.layout_el2);
+
 /// Stack Pointer (EL2)
 // TODO: Should this be a pointer to some larger size to enforce stack
 // pointer alignment?
@@ -41,34 +42,50 @@ pub const SPSR_EL2 = R("SPSR_EL2", spsr.layout);
 pub const ELR_EL1 = R("ELR_EL1", u64);
 
 /// System Control Register (EL1)
-pub const SCTLR_EL1 = R("SCTLR_EL1", sctlr_el1.layout);
+pub const SCTLR_EL1 = R("SCTLR_EL1", sctlr.layout_el1);
 
 /// Stack Pointer (EL1)
 pub const SP_EL1 = R("SP_EL1", *u8);
+
+/// Saved Program Status Register (EL1)
+pub const SPSR_EL1 = R("SPSR_EL1", spsr.layout);
 
 // ----------------------------------------------------------------------
 // EL0 (Application)
 // ----------------------------------------------------------------------
 
-// Counter-timer physical control register
-pub const CNTP_CTL_EL0 = R("CNTP_CTL_EL0", cntp_ctl.layout);
-
-// Counter-timer physical compare value register
-pub const CNTP_CVAL_EL0 = R("CNTP_CVAL_EL0", u64);
-
-// Counter-timer timer value register
-pub const CNTP_TVAL_EL0 = R("CNTP_TVAL_EL0", u64);
-
-// System clock frequency (in Hz)
+/// System clock frequency (in Hz)
 // TODO: technically this is a read only register. Should we have a
 // separate type for that?
 pub const CNTFRQ_EL0 = R("CNTFRQ_EL0", u64);
 
-// Physical count value
+/// Physical count value
 pub const CNTPCT_EL0 = R("CNTPCT_EL0", u64);
+
+/// Counter-timer physical control register
+pub const CNTP_CTL_EL0 = R("CNTP_CTL_EL0", cntp_ctl.layout);
+
+/// Counter-timer physical compare value register
+pub const CNTP_CVAL_EL0 = R("CNTP_CVAL_EL0", u64);
+
+/// Counter-timer physical timer value register
+/// This is a 64-bit register but only the bottom 32 bits will ever
+/// have a value.
+pub const CNTP_TVAL_EL0 = R("CNTP_TVAL_EL0", u64);
+
+/// Counter-timer virtual timer control register
+pub const CNTV_CTL_EL0 = R("CNTV_CTL_EL0", cntv_ctl.layout);
+
+/// Counter-timer virtual timer register
+pub const CNTV_CVAL_EL0 = R("CNTV_CVAL_EL0", u64);
+
+/// Counter-timer virtual timer value register
+/// This is a 64-bit register but only the bottom 32 bits will ever
+/// have a value.
+pub const CNTV_TVAL_EL0 = R("CNTV_TVAL_EL0", u64);
 
 // TODO: another read-only register
 pub const CNTVCT_EL0 = R("CNTVCT_EL0", u64);
 
-/// Saved Program Status Register (EL1)
-pub const SPSR_EL1 = R("SPSR_EL1", spsr.layout);
+/// Stack Pointer (EL0)
+pub const SP_EL0 = R("SP_EL0", *u8);
