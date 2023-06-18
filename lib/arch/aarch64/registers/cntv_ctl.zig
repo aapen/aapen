dda5a3486690b87fa6b28900615d1bf747e4f81c
Michael Nygard <mtnygard@gmail.com>
2023-06-18 10:40:16 -0500

    Missing files.

---
+++ lib/arch/aarch64/registers/cntv_ctl.zig
@@ -0,0 +1,18 @@
+/// Counter-timer Virtual Timer Control register
+const types = @import("../system_register.zig");
+
+pub const layout = packed struct {
+    enable: enum(u1) {
+        disable = 0,
+        enable = 1,
+    },
+    imask: enum(u1) {
+        not_masked = 0,
+        masked = 1,
+    },
+    istatus: enum(u1) {
+        not_met = 0,
+        met = 1,
+    },
+    _unused_reserved: u61 = 0,
+};
