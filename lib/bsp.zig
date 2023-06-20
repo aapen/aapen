d39857628210868d8a6b94cd95b40aa10873507d
Michael Nygard <mtnygard@gmail.com>
2023-06-20 06:47:08 -0500

    Some notes and todos

--- lib/bsp.zig
+++ lib/bsp.zig
@@ -1,1 +1,3 @@
+// TODO: Choose the value of `io` based on the boot-time hardware.
+// TODO (alternative): Choose the value of `io` based on a comptime definition.
 pub const io = @import("bsp/raspi3/io.zig");
