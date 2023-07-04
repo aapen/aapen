be93c8955998e9fd2da812d258bb2968a8bdc140
Michael Nygard <michael.nygard@nubank.com.br>
2023-07-04 18:37:06 +0000

    Function comment

--- src/util.s
+++ src/util.s
@@ -1,4 +1,15 @@
-.global spin_delay
+        .section .text.util
+        
+// ----------------------------------------------------------------------
+// fn spin_delay()
+//
+// Arguments:
+//      x0 - number of cycles to spin for
+// Returns: none
+// Clobbers: x0
+// ----------------------------------------------------------------------
+        .global spin_delay
+        .type spin_delay, @function
 spin_delay:
     subs x0, x0, #1
     bne spin_delay
