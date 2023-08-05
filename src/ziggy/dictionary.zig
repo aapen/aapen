fa0e0230a6b9fb46629d03397bbb27c2ec9dfcff
Russ Olsen <russ@cognitect.com>
2023-08-05 09:15:27 -0400

    Search the dictionary backwards.

--- src/ziggy/dictionary.zig
+++ src/ziggy/dictionary.zig
@@ -62,12 +62,15 @@ pub fn Dictionary(comptime DType: type) type {
             }
 
             // TODO - how to iterate this in reverse?
-            for (self.contents.items) |entry| {
+            var i: i32 = @as(i32, @intCast(self.contents.items.len)) - 1;
+            while (i >= 0) {
+                const entry = self.contents.items[@intCast(i)];
                 var this_name = entry.name;
                 var l = std.mem.indexOfSentinel(u8, 0, &this_name);
                 if (std.mem.eql(u8, this_name[0..l], name)) {
                     return entry;
                 }
+                i -= 1;
             }
 
             return ForthError.NotFound;
@@ -87,19 +90,27 @@ test "Basic dictionary operation" {
     var allocator = gpa.allocator();
 
     const print = std.debug.print;
+    const assert = std.debug.assert;
 
     const D1 = Dictionary(i32);
 
     var d = D1.init(allocator);
     defer d.deinit();
 
+    assert(d.isEmpty());
+
     try d.put("foo", 1234, false);
     try d.put("bar", 7717, false);
+
+    assert(!d.isEmpty());
+
     var v = try d.get("foo");
     print("rop: {}\n", .{v});
+    assert(v == 1234);
 
     v = try d.get("bar");
     print("bar: {}\n", .{v});
+    assert(v == 7717);
 
     _ = try d.put("aaa", 111, false);
     _ = try d.put("bbb", 222, false);
@@ -107,9 +118,10 @@ test "Basic dictionary operation" {
 
     v = try d.get("bbb");
     print("bbb: {}\n", .{v});
+    assert(v == 212);
 
     v = d.get("zzzz") catch 999999;
     print("zzz: {}\n", .{v});
 
-    try d.pr(std.debug.print);
+    //try d.pr(std.debug.print);
 }
