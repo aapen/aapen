8d109ca6e24baa59ee4b478032b9f56c55a86459
Michael Nygard <mtnygard@gmail.com>
2023-08-06 07:47:46 -0500

    Add input word `key`
    This commit has some spurious whitespace changes.  We might have
    different settings for `zig format`. I'm using the default settings
    from https://github.com/ziglang/zig-mode, which is 4 spaces and uses
    format-on-save.

--- src/ziggy/core.zig
+++ src/ziggy/core.zig
@@ -50,6 +50,13 @@ pub fn wordEmit(forth: *Forth) !void {
     forth.console.emit(ch);
 }
 
+// -- ch
+pub fn wordKey(forth: *Forth) !void {
+    var s = &forth.stack;
+    var ch = forth.getc();
+    try s.push(Value{ .ch = ch });
+}
+
 /// --
 pub fn wordCr(forth: *Forth) ForthError!void {
     forth.putc(0x0a);
@@ -91,7 +98,6 @@ pub fn wordRStack(forth: *Forth) !void {
         try forth.print("{}\n", .{item});
     }
 }
-
 
 /// w1 w2 -- w2 w1
 pub fn wordSwap(forth: *Forth) !void {
@@ -208,7 +214,6 @@ pub fn wordAdd(forth: *Forth) !void {
     try s.push(try a.add(&b));
 }
 
-
 /// n n -- n
 pub fn wordSub(forth: *Forth) !void {
     var s = &forth.stack;
@@ -285,42 +290,42 @@ pub fn wordValueSize(forth: *Forth) ForthError!void {
 }
 
 pub fn defineCore(forth: *Forth) !void {
-  // Screen control
-  try forth.definePrimitive("emit", &wordEmit, false);
-  try forth.definePrimitive("cls", &wordClearScreen, false);
+    // IO
+    try forth.definePrimitive("hello", &wordHello, false);
+    try forth.definePrimitive("cr", &wordCr, false);
+    try forth.definePrimitive("emit", &wordEmit, false);
+    try forth.definePrimitive("cls", &wordClearScreen, false);
+    try forth.definePrimitive("key", &wordKey, false);
 
-  // Secondary definition words.
-  try forth.definePrimitive(":", &wordColon, false);
-  try forth.definePrimitive(";", &wordSemi, true);
+    // Secondary definition words.
+    try forth.definePrimitive(":", &wordColon, false);
+    try forth.definePrimitive(";", &wordSemi, true);
 
-  // Debug and inspection words.
-  try forth.definePrimitive("stack", &wordStack, false);
-  try forth.definePrimitive("?", &wordStack, false);
-  try forth.definePrimitive("??", &wordDictionary, false);
-  try forth.definePrimitive("rstack", &wordRStack, false);
-  try forth.definePrimitive("info", &wordInfo, true);
-  try forth.definePrimitive("ip", &wordNext, false);
-  try forth.definePrimitive("value-size", &wordValueSize, false);
+    // Debug and inspection words.
+    try forth.definePrimitive("stack", &wordStack, false);
+    try forth.definePrimitive("?", &wordStack, false);
+    try forth.definePrimitive("??", &wordDictionary, false);
+    try forth.definePrimitive("rstack", &wordRStack, false);
+    try forth.definePrimitive("info", &wordInfo, true);
+    try forth.definePrimitive("ip", &wordNext, false);
+    try forth.definePrimitive("value-size", &wordValueSize, false);
 
-  // Basic Forth words.
-  try forth.definePrimitive("swap", &wordSwap, false);
-  try forth.definePrimitive("2swap", &word2Swap, false);
-  try forth.definePrimitive("dup", &wordDup, false);
-  try forth.definePrimitive("2dup", &word2Dup, false);
-  try forth.definePrimitive("drop", &wordDrop, false);
-  try forth.definePrimitive("2drop", &word2Drop, false);
-  try forth.definePrimitive("rot", &wordRot, false);
-  try forth.definePrimitive("2rot", &word2Rot, false);
-  try forth.definePrimitive("over", &wordOver, false);
-  try forth.definePrimitive("2over", &word2Over, false);
+    // Basic Forth words.
+    try forth.definePrimitive("swap", &wordSwap, false);
+    try forth.definePrimitive("2swap", &word2Swap, false);
+    try forth.definePrimitive("dup", &wordDup, false);
+    try forth.definePrimitive("2dup", &word2Dup, false);
+    try forth.definePrimitive("drop", &wordDrop, false);
+    try forth.definePrimitive("2drop", &word2Drop, false);
+    try forth.definePrimitive("rot", &wordRot, false);
+    try forth.definePrimitive("2rot", &word2Rot, false);
+    try forth.definePrimitive("over", &wordOver, false);
+    try forth.definePrimitive("2over", &word2Over, false);
 
-  try forth.definePrimitive("hello", &wordHello, false);
-  try forth.definePrimitive(".", &wordDot, false);
-  try forth.definePrimitive("h.", &wordHexDot, false);
-  try forth.definePrimitive("cr", &wordCr, false);
-  try forth.definePrimitive("+", &wordAdd, false);
-  try forth.definePrimitive("-", &wordSub, false);
-  try forth.definePrimitive("!i", &wordStoreI32, false);
-  try forth.definePrimitive("@i", &wordLoadI32, false);
+    try forth.definePrimitive(".", &wordDot, false);
+    try forth.definePrimitive("h.", &wordHexDot, false);
+    try forth.definePrimitive("+", &wordAdd, false);
+    try forth.definePrimitive("-", &wordSub, false);
+    try forth.definePrimitive("!i", &wordStoreI32, false);
+    try forth.definePrimitive("@i", &wordLoadI32, false);
 }
-
