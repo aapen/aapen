1882e007ae2449686bf58a8c78872e5b5205fda9
Russ Olsen <russ@cognitect.com>
2023-09-23 14:12:02 -0400

    Add string.zig tests back in.

--- src/tests.zig
+++ src/tests.zig
@@ -3,7 +3,7 @@ pub const devicetree = @import("devicetree.zig");
 pub const ring = @import("ring.zig");
 pub const bcd = @import("bcd.zig");
 pub const forty_stack = @import("forty/stack.zig");
-//pub const forty_string = @import("forty/string.zig");
+pub const forty_string = @import("forty/string.zig");
 
 test "parent of all tests" {
     std.debug.print("\n", .{});
