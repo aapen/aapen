8e277a0a7a16f5d07854eb853ac935276fda753d
Michael Nygard <michael.nygard@nubank.com.br>
2023-10-21 17:47:35 -0300

    Build time config for target kernel files
    New option to `zig build`: -Dimage
    
    Defaults to `kernel8` which results in building `zig-out/kernel8.elf`
    and `zig-out/kernel8.img`.

--- build.zig
+++ build.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const fmt = std.fmt;
+
 const Build = std.Build;
 const Target = std.Target;
 const CrossTarget = std.zig.CrossTarget;
@@ -20,7 +22,18 @@ fn configModule(b: *std.Build) *Module {
     return b.createModule(.{ .source_file = .{ .path = config_path } });
 }
 
-pub fn build(b: *std.Build) void {
+fn kernelFile(b: *std.Build) []const u8 {
+    const maybe_target_file = b.option([]const u8, "image", "Base name for the kernel binaries (both elf and img)");
+
+    return maybe_target_file orelse "kernel8";
+}
+
+pub fn build(b: *std.Build) !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
     const target = std.zig.CrossTarget{
         .cpu_arch = Target.Cpu.Arch.aarch64,
         .os_tag = Target.Os.Tag.freestanding,
@@ -28,9 +41,15 @@ pub fn build(b: *std.Build) void {
     };
 
     const optimize = b.standardOptimizeOption(.{});
+    const bin_basename = kernelFile(b);
+    const elf_name = try fmt.allocPrint(allocator, "{s}.elf", .{bin_basename});
+    defer allocator.free(elf_name);
+
+    const img_name = try fmt.allocPrint(allocator, "{s}.img", .{bin_basename});
+    defer allocator.free(img_name);
 
     const kernel = b.addExecutable(.{
-        .name = "kernel8.elf",
+        .name = elf_name,
         .target = target,
         .root_source_file = .{ .path = "src/main.zig" },
         .optimize = optimize,
@@ -50,16 +69,15 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(kernel);
 
     const objcopy = b.addObjCopy(kernel.getOutputSource(), .{
-        .basename = "./kernel8.img",
         .format = std.Build.Step.ObjCopy.RawFormat.bin,
     });
 
     objcopy.step.dependOn(&kernel.step);
 
-    const install_elf = b.addInstallFile(kernel.getOutputSource(), "kernel8.elf");
+    const install_elf = b.addInstallFile(kernel.getOutputSource(), elf_name);
     b.getInstallStep().dependOn(&install_elf.step);
 
-    const install_obj = b.addInstallFile(objcopy.getOutputSource(), "kernel8.img");
+    const install_obj = b.addInstallFile(objcopy.getOutputSource(), img_name);
     b.getInstallStep().dependOn(&install_obj.step);
 }
 
