8433f311d770c2edc185463906204f8cc1dd6f56
Michael Nygard <mtnygard@gmail.com>
2023-10-31 07:12:11 -0500

    Continued cleanup of debug.zig
    It always bothered me that there were CPU-specific parts in debug. Now
    with the std.log change, it's cleaner to move the display
    functions (which depend on the CPU architecture) into exceptions.zig

--- src/arch/aarch64/exceptions.zig
+++ src/arch/aarch64/exceptions.zig
@@ -1,4 +1,8 @@
+const std = @import("std");
+
 const root = @import("root");
+const kprint = root.kprint;
+
 const debug = @import("../../debug.zig");
 const cpu = @import("../../architecture.zig").cpu;
 const hal = @import("../../hal.zig");
@@ -47,42 +51,6 @@ pub const ExceptionContext = struct {
     force_sp: u64,
 };
 
-// TODO Seems odd to have a dependency from the CPU-specific module to
-// the screen object. Should this be injected? If so, how?
-export fn invalidEntryMessageShow(context: *ExceptionContext, entry_type: u64) void {
-    // Check if this was a breakpoint due to std.builtin.default_panic
-    if (context.esr.ec == .brk) {
-        // Breakpoint number is the lower 16 bits of ESR's ISS
-        var breakpoint_number: u16 = @truncate(context.esr.iss & 0xffff);
-
-        // Zig uses 0xf000 to indicate a panic
-        if (breakpoint_number == 0xf000) {
-            // Could we get the panic string and arguments from the
-            // stack?
-            debug.panicDisplay(context.elr);
-            var unwind = unwindPointLocate(context);
-            if (unwind.sp != undefined) {
-                context.elr = unwind.pc;
-                context.force_sp = unwind.sp;
-                context.lr = unwind.lr;
-                context.gpr[29] = unwind.fp;
-            }
-        } else if (breakpoint_number == soft_reset_breakpoint) {
-            root.resetSoft();
-
-            // Adjust ELR to resume execution _after_ the breakpoint instruction
-            context.elr += 4;
-        } else {
-            debug.unknownBreakpointDisplay(context.elr, breakpoint_number);
-
-            // Adjust ELR to resume execution _after_ the breakpoint instruction
-            context.elr += 4;
-        }
-    } else {
-        debug.unhandledExceptionDisplay(context.elr, entry_type, @as(u64, @bitCast(context.esr)), context.esr.ec);
-    }
-}
-
 fn unwindPointLocate(_: *ExceptionContext) *UnwindPoint {
     return &root.global_unwind_point;
 }
@@ -104,4 +72,88 @@ const soft_reset_breakpoint = 0x7c5;
 
 pub fn triggerSoftReset() void {
     asm volatile ("brk 0x7c5");
+}
+
+// ----------------------------------------------------------------------
+// Exception display
+// ----------------------------------------------------------------------
+
+export fn invalidEntryMessageShow(context: *ExceptionContext, entry_type: u64) void {
+    // Check if this was a breakpoint due to std.builtin.default_panic
+    if (context.esr.ec == .brk) {
+        // Breakpoint number is the lower 16 bits of ESR's ISS
+        var breakpoint_number: u16 = @truncate(context.esr.iss & 0xffff);
+
+        // Zig uses 0xf000 to indicate a panic
+        if (breakpoint_number == 0xf000) {
+            // Could we get the panic string and arguments from the
+            // stack?
+            panicDisplay(context.elr);
+            var unwind = unwindPointLocate(context);
+            if (unwind.sp != undefined) {
+                context.elr = unwind.pc;
+                context.force_sp = unwind.sp;
+                context.lr = unwind.lr;
+                context.gpr[29] = unwind.fp;
+            }
+        } else if (breakpoint_number == soft_reset_breakpoint) {
+            root.resetSoft();
+
+            // Adjust ELR to resume execution _after_ the breakpoint instruction
+            context.elr += 4;
+        } else {
+            unknownBreakpointDisplay(context.elr, breakpoint_number);
+
+            // Adjust ELR to resume execution _after_ the breakpoint instruction
+            context.elr += 4;
+        }
+    } else {
+        unhandledExceptionDisplay(context.elr, entry_type, @as(u64, @bitCast(context.esr)), context.esr.ec);
+    }
+}
+
+fn panicDisplay(elr: ?u64) void {
+    if (elr) |addr| {
+        kprint("Panic!\nELR: 0x{x:0>8}\n", .{addr});
+        stackTraceDisplay(addr);
+    } else {
+        kprint("Panic!\nSource unknown.\n", .{});
+    }
+}
+
+fn unknownBreakpointDisplay(from_addr: ?u64, bkpt_number: u16) void {
+    if (from_addr) |addr| {
+        kprint("Breakpoint\nComment: 0x{x:0>8}\n ELR: 0x{x:0>8}\n", .{ bkpt_number, addr });
+    } else {
+        kprint("Breakpoint\nComment: 0x{x:0>8}\n ELR: unknown\n", .{bkpt_number});
+    }
+}
+
+fn unhandledExceptionDisplay(from_addr: ?u64, entry_type: u64, esr: u64, ec: cpu.registers.EC) void {
+    if (from_addr) |addr| {
+        kprint("Unhandled exception!\nType: 0x{x:0>8}\n ESR: 0x{x:0>8}\n ELR: 0x{x:0>8}\n  EC: {s}\n", .{ entry_type, @as(u64, @bitCast(esr)), addr, @tagName(ec) });
+    } else {
+        kprint("Unhandled exception!\nType: 0x{x:0>8}\n ESR: 0x{x:0>8}\n ELR: unknown\n  EC: 0b{b:0>6}\n", .{ entry_type, esr, @tagName(ec) });
+    }
+}
+
+fn stackTraceDisplay(from_addr: u64) void {
+    _ = from_addr;
+    var it = std.debug.StackIterator.init(null, null);
+    defer it.deinit();
+
+    kprint("\nStack trace\n", .{});
+    kprint("Frame\tPC\n", .{});
+    for (0..40) |i| {
+        var addr = it.next() orelse {
+            kprint(".\n", .{});
+            return;
+        };
+        stackFrameDisplay(i, addr);
+    }
+    kprint("--stack trace truncated--\n", .{});
+}
+
+fn stackFrameDisplay(frame_number: usize, frame_pointer: usize) void {
+    kprint("{d}\t0x{x:0>8}\n", .{ frame_number, frame_pointer });
 }
--- src/debug.zig
+++ src/debug.zig
@@ -39,56 +39,8 @@ pub fn kprint(comptime fmt: []const u8, args: anytype) void {
     }
 
     if (root.console_valid) {
-        root.frame_buffer_console.print(fmt, args) catch {
-            // not much we can do here
-        };
+        root.frame_buffer_console.print(fmt, args) catch {};
     }
-}
-
-pub fn panicDisplay(elr: ?u64) void {
-    if (elr) |addr| {
-        kprint("Panic!\nELR: 0x{x:0>8}\n", .{addr});
-        stackTraceDisplay(addr);
-    } else {
-        kprint("Panic!\nSource unknown.\n", .{});
-    }
-}
-
-pub fn unknownBreakpointDisplay(from_addr: ?u64, bkpt_number: u16) void {
-    if (from_addr) |addr| {
-        kprint("Breakpoint\nComment: 0x{x:0>8}\n ELR: 0x{x:0>8}\n", .{ bkpt_number, addr });
-    } else {
-        kprint("Breakpoint\nComment: 0x{x:0>8}\n ELR: unknown\n", .{bkpt_number});
-    }
-}
-
-pub fn unhandledExceptionDisplay(from_addr: ?u64, entry_type: u64, esr: u64, ec: arch.cpu.registers.EC) void {
-    if (from_addr) |addr| {
-        kprint("Unhandled exception!\nType: 0x{x:0>8}\n ESR: 0x{x:0>8}\n ELR: 0x{x:0>8}\n  EC: {s}\n", .{ entry_type, @as(u64, @bitCast(esr)), addr, @tagName(ec) });
-    } else {
-        kprint("Unhandled exception!\nType: 0x{x:0>8}\n ESR: 0x{x:0>8}\n ELR: unknown\n  EC: 0b{b:0>6}\n", .{ entry_type, esr, @tagName(ec) });
-    }
-}
-
-fn stackTraceDisplay(from_addr: u64) void {
-    _ = from_addr;
-    var it = std.debug.StackIterator.init(null, null);
-    defer it.deinit();
-
-    kprint("\nStack trace\n", .{});
-    kprint("Frame\tPC\n", .{});
-    for (0..40) |i| {
-        var addr = it.next() orelse {
-            kprint(".\n", .{});
-            return;
-        };
-        stackFrameDisplay(i, addr);
-    }
-    kprint("--stack trace truncated--\n", .{});
-}
-
-fn stackFrameDisplay(frame_number: usize, frame_pointer: usize) void {
-    kprint("{d}\t0x{x:0>8}\n", .{ frame_number, frame_pointer });
 }
 
 // ------------------------------------------------------------------------------
