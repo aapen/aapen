240212cee94b5e53af4892286f012bf4ac863044
Michael Nygard <mtnygard@gmail.com>
2024-01-28 11:18:29 -0600

    Fix boot problem with MAIR
    Preprocessor macros and parentheses.
    
    LDR_IMM64 wasn't isolating its value well enough during expansion. So
    if the value itself wasn't a single token (narrator: "it wasn't") then
    the logic in the macro gets broken.

--- include/asm/macro.h
+++ include/asm/macro.h
@@ -37,20 +37,20 @@ name:                                           \
 // Generate the optimal set of instructions to load a 64-bit immediate
 // value into a register.
 .macro  LDR_IMM64 reg,value
-  .if \value > 0xffffffffffff
-  movz \reg, #(\value & 0xffff)
-  movk \reg, #((\value >> 16) & 0xffff), lsl #16
-  movk \reg, #((\value >> 32) & 0xffff), lsl #32
-  movk \reg, #((\value >> 48) & 0xffff), lsl #48
-  .elseif \value > 0xffffffff
-  movz \reg, #(\value & 0xffff)
-  movk \reg, #((\value >> 16) & 0xffff), lsl #16
-  movk \reg, #((\value >> 32) & 0xffff), lsl #32
-  .elseif \value > 0xffff
-  movz \reg, #(\value & 0xffff)
-  movk \reg, #((\value >> 16) & 0xffff), lsl #16
-  .elseif \value > 0
-  movz \reg, #(\value & 0xffff)
+  .if (\value) > 0xffffffffffff
+  movz \reg, #((\value) & 0xffff)
+  movk \reg, #(((\value) >> 16) & 0xffff), lsl #16
+  movk \reg, #(((\value) >> 32) & 0xffff), lsl #32
+  movk \reg, #(((\value) >> 48) & 0xffff), lsl #48
+  .elseif (\value) > 0xffffffff
+  movz \reg, #((\value) & 0xffff)
+  movk \reg, #(((\value) >> 16) & 0xffff), lsl #16
+  movk \reg, #(((\value) >> 32) & 0xffff), lsl #32
+  .elseif (\value) > 0xffff
+  movz \reg, #((\value) & 0xffff)
+  movk \reg, #(((\value) >> 16) & 0xffff), lsl #16
+  .elseif (\value) > 0
+  movz \reg, #((\value) & 0xffff)
   .elseif
   movz \reg, #0
   .endif
