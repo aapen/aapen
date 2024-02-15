aapen/aapen

commit c5cd3d2e5b5ff30494f480a8d6bc76adb025f9a9

Fix memory leak with DMA

src/drivers/bcm_dma.zig
@@ -142,6 +142,7 @@ interrupt_status: *volatile u32,
 transfer_enabled: *volatile u32,
 intc: *InterruptController,
 channels: DmaChannels,
+channel_control_blocks: [max_channel_id]BroadcomDMAControlBlock,
 
 pub fn init(
     allocator: Allocator,
@@ -157,6 +158,7 @@ pub fn init(
         .transfer_enabled = @ptrFromInt(register_base + 0xff0),
         .translations = translations,
         .channels = .{},
+        .channel_control_blocks = undefined,
     };
 }
 
@@ -190,27 +192,10 @@ pub fn reserveChannel(self: *Self) DMAError!Channel {
     };
 }
 
-pub fn createRequest(self: *Self) DMAError!*Request {
-    var request = try self.allocator.create(Request);
-    request.control_blocks = null;
-    return request;
-}
-
-pub fn destroyRequest(self: *Self, request: *Request) void {
-    if (request.control_blocks) |cb_slice| {
-        self.allocator.free(cb_slice);
-    }
-
-    self.allocator.destroy(request);
-}
-
 // TODO: after dma completes, free the control block
 pub fn initiate(self: *Self, channel: Channel, request: *Request) DMAError!void {
-    const cb_slice = try self.allocator.alignedAlloc(BroadcomDMAControlBlock, 32, 1);
-    request.control_blocks = cb_slice;
-
     const channel_registers = channel.registers;
-    const control_block = &cb_slice[0];
+    const control_block = &self.channel_control_blocks[channel.channel_id];
     const mode_2d: u1 = if (request.stride != 0) 1 else 0;
 
     control_block.* = .{

src/frame_buffer.zig
@@ -245,21 +245,15 @@ pub fn fill(fb: *Self, left: usize, top: usize, right: usize, bottom: usize, col
     const b = clamp(usize, 0, bottom, fb.yres);
 
     if (fb.dma_channel) |ch| {
-        if (fb.dma.createRequest()) |req| {
-            // DMA fill
-            defer fb.dma.destroyRequest(req);
-            fb.fillDMA(ch, req, l, t, r, b, color);
-            return;
-        } else |_| {
-            // will use fallback
-        }
+        // if we have DMA, use it
+        fb.fillDMA(ch, l, t, r, b, color);
+    } else {
+        // fallback to CPU driven fill
+        fb.fillPixels(l, t, r, b, color);
     }
-
-    // fallback to CPU driven fill
-    fb.fillPixels(l, t, r, b, color);
 }
 
-fn fillDMA(fb: *Self, ch: DMAChannel, req: *DMARequest, l: usize, t: usize, r: usize, b: usize, color: u8) void {
+fn fillDMA(fb: *Self, ch: DMAChannel, l: usize, t: usize, r: usize, b: usize, color: u8) void {
     var cvec: @Vector(16, u8) = @splat(color);
 
     const fb_base: usize = @intFromPtr(fb.base);
@@ -279,7 +273,7 @@ fn fillDMA(fb: *Self, ch: DMAChannel, req: *DMARequest, l: usize, t: usize, r: u
     else
         ((b - t) * fb.xres);
 
-    req.* = .{
+    var req = DMARequest{
         .source = @truncate(src),
         .source_increment = false,
         .destination = @truncate(dest),
@@ -287,7 +281,7 @@ fn fillDMA(fb: *Self, ch: DMAChannel, req: *DMARequest, l: usize, t: usize, r: u
         .length = xfer_count,
         .stride = (dest_stride << 16) | src_stride,
     };
-    fb.dma.initiate(ch, req) catch {};
+    fb.dma.initiate(ch, &req) catch {};
     _ = fb.dma.awaitChannel(ch);
 }
 

commit 02a66d0cd08a0bc024b9cc42260564d9acc7cdc7

Restore DMA
It was an alignment issue with the statically allocated control blocks
I introduced in c5cd3d2

src/drivers/bcm_dma.zig
@@ -142,7 +142,7 @@ interrupt_status: *volatile u32,
 transfer_enabled: *volatile u32,
 intc: *InterruptController,
 channels: DmaChannels,
-channel_control_blocks: [max_channel_id]BroadcomDMAControlBlock,
+channel_control_blocks: [max_channel_id]BroadcomDMAControlBlock align(32),
 
 pub fn init(
     allocator: Allocator,