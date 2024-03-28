c5e47c49b935d8032eb581249513ee981f3cfd80
Michael Nygard <mtnygard@gmail.com>
2024-03-28 07:28:37 -0500

    Introduce word `address-of` to get the address of a symbol

--- src/debug.zig
+++ src/debug.zig
@@ -29,6 +29,7 @@ pub fn defineModule(forth: *Forth) !void {
     try forth.defineConstant("debug-info-valid", @intFromPtr(&debug_info_valid));
     try forth.defineNamespace(@This(), .{
         .{ "lookupSymbol", "symbol-at" },
+        .{ "addressOf", "address-of" },
     });
 }
 
@@ -60,6 +61,78 @@ const DebugInfoHeader = extern struct {
     padding: u32,
 };
 
+const Symbol = struct {
+    low_pc: u64,
+    high_pc: u64,
+    symbol_offset: u64,
+
+    const none: Symbol = .{ .low_pc = 0, .high_pc = std.math.maxInt(u64), .symbol_offset = 0 };
+
+    pub fn lookupByAddr(addr: u64) ?*const Symbol {
+        if (!debug_info_valid) {
+            _ = printf("debug info not available\n");
+            return null;
+        }
+
+        var best_match = &Symbol.none;
+        for (debug_symbols) |*symb| {
+            if (symb.contains(addr)) {
+                if (best_match.isWider(symb)) {
+                    best_match = symb;
+                }
+            }
+        }
+
+        return if (best_match == &Symbol.none) null else best_match;
+    }
+
+    pub fn lookupByName(nm: [*:0]const u8) ?*const Symbol {
+        if (!debug_info_valid) {
+            _ = printf("debug info not available\n");
+            return null;
+        }
+
+        for (debug_symbols) |*symb| {
+            if (symb.nameMatches(nm)) {
+                return symb;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    pub fn name(symbol: *const Symbol) [*:0]const u8 {
+        return @ptrCast(debug_strings.? + symbol.symbol_offset);
+    }
+
+    fn contains(symbol: *const Symbol, addr: u64) bool {
+        return symbol.low_pc <= addr and addr < symbol.high_pc;
+    }
+
+    fn nameMatches(symbol: *const Symbol, b: [*:0]const u8) bool {
+        const a: [*:0]const u8 = symbol.name();
+        var i: usize = 0;
+        while (true) {
+            if (a[i] == 0 and b[i] == 0) return true; // same length
+            if (a[i] != b[i]) return false; // different char
+            if (a[i] == 0 or b[i] == 0) return false; // different lengths
+            i += 1;
+        }
+    }
+
+    fn span(symbol: *const Symbol) usize {
+        if (symbol.high_pc > symbol.low_pc) {
+            return symbol.high_pc - symbol.low_pc;
+        } else {
+            return std.math.maxInt(usize);
+        }
+    }
+
+    fn isWider(this: *const Symbol, that: *const Symbol) bool {
+        return this.span() > that.span();
+    }
+};
+
 pub fn initDebugInfo() void {
     debug_info_valid = true;
 
@@ -73,50 +146,21 @@ pub fn initDebugInfo() void {
     debug_strings = @ptrFromInt(string_locations);
 }
 
-const Symbol = struct {
-    low_pc: u64,
-    high_pc: u64,
-    symbol_offset: u64,
-
-    const none: Symbol = .{ .low_pc = 0, .high_pc = std.math.maxInt(u64), .symbol_offset = 0 };
-
-    pub fn contains(symbol: *const Symbol, addr: u64) bool {
-        return symbol.low_pc <= addr and addr < symbol.high_pc;
-    }
-
-    pub fn span(symbol: *const Symbol) usize {
-        if (symbol.high_pc > symbol.low_pc) {
-            return symbol.high_pc - symbol.low_pc;
-        } else {
-            return std.math.maxInt(usize);
-        }
-    }
-
-    pub fn isWider(this: *const Symbol, that: *const Symbol) bool {
-        return this.span() > that.span();
-    }
-};
-
 pub fn lookupSymbol(addr: u64) ?[*:0]const u8 {
-    if (!debug_info_valid) {
-        _ = printf("debug info not available\n");
-        return null;
-    }
-
-    var best_match = &Symbol.none;
-    for (debug_symbols) |*symb| {
-        if (symb.contains(addr)) {
-            if (best_match.isWider(symb)) {
-                best_match = symb;
-            }
-        }
-    }
-
-    if (best_match == &Symbol.none) {
+    if (Symbol.lookupByAddr(addr)) |found| {
+        return found.name();
+    } else {
         _ = printf("not found\n");
         return null;
+    }
+}
+
+pub fn addressOf(name: [*:0]const u8) u64 {
+    if (Symbol.lookupByName(name)) |found| {
+        return found.low_pc;
     } else {
-        return @ptrCast(debug_strings.? + best_match.symbol_offset);
+        _ = printf("not found\n");
+        return 0;
     }
 }
 
