f1aeecd31c3a778dac4cc0b0cd8e51c3bf5d6108
Michael Nygard <mtnygard@gmail.com>
2024-03-31 11:02:34 -0500

    Credit to the author of the Docker image

--- tools/qemu_raspios.sh
+++ tools/qemu_raspios.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
+#
+# See https://interrupt.memfault.com/blog/emulating-raspberry-pi-in-qemu
 
 docker run -it --rm -p 2222:2222 --security-opt seccomp=unconfined stawiski/qemu-raspberrypi-3b:2023-05-03-raspios-bullseye-arm64
