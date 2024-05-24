038823a33ca00229f6d7dbbe340768fcc01cf9c6
Michael Nygard <michael.nygard@nubank.com.br>
2024-05-24 11:51:12 -0300

    Make mouse coords easier to access
    We have Forty words to peek a byte, a word, and a double word. Just
    not for an i16. So easiest fix is to make the mouse variables i32.

--- src/mouse.zig
+++ src/mouse.zig
@@ -7,19 +7,11 @@ pub fn defineModule(forth: *Forth) !void {
     try forth.defineConstant("mouse-buttons", @intFromPtr(&buttons));
 }
 
-pub fn getMouseX() i16 {
-    return x;
-}
+pub var x_max: i32 = @intCast(fb.DEFAULT_X_RESOLUTION);
+pub var y_max: i32 = @intCast(fb.DEFAULT_Y_RESOLUTION);
 
-pub fn getMosueY() i16 {
-    return y;
-}
-
-pub var x_max: i16 = @intCast(fb.DEFAULT_X_RESOLUTION);
-pub var y_max: i16 = @intCast(fb.DEFAULT_Y_RESOLUTION);
-
-pub var x: i16 = 0;
-pub var y: i16 = 0;
+pub var x: i32 = 0;
+pub var y: i32 = 0;
 pub var dx: i8 = 0;
 pub var dy: i8 = 0;
 pub var buttons: u8 = 0;
@@ -28,8 +20,8 @@ pub fn update(new_buttons: u8, x_move: i8, y_move: i8) void {
     buttons = new_buttons;
     dx = x_move;
     dy = y_move;
-    x = clamp(i16, 0, x + dx, x_max);
-    y = clamp(i16, 0, y + dy, y_max);
+    x = clamp(i32, 0, x + dx, x_max);
+    y = clamp(i32, 0, y + dy, y_max);
 }
 
 inline fn clamp(comptime T: type, min: T, val: T, max: T) T {
