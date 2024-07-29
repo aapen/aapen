966d9a28db823d334728018ecb290eb3ea8c2d51
Russ Olsen <russ@cognitect.com>
2024-07-29 15:37:16 -0400

    Cleared up some literal ints, add a few more instructions.

--- src/assembler.f
+++ src/assembler.f
@@ -4,6 +4,10 @@ noecho
 
 ( Utility words that set fields on the instructions. )
 
+: set-32bit ( instruction -- instruction )
+  0x 3fffffff and		( clear bits 31 and 30)
+;
+
 : set-rt ( instruction rt - instruction )
   or
 ;
@@ -19,12 +23,12 @@ noecho
 ;
 
 : set-im21-hi ( instruction im21 -- instruction )
-  2 rshift 5 lshift             ( instruction im21-hibits )
+  0d 2 rshift 0d 5 lshift       ( instruction im21-hibits )
   or
 ;
 
 : set-im21-low ( instruction im21 -- instruction )
-  0x 3 and 29 lshift		( instruction im21-lowbits )
+  0x 3 and 0d 29 lshift		( instruction im21-lowbits )
   or
 ;
 
@@ -34,31 +38,64 @@ noecho
   swap set-im21-low             ( instruction )
 ;
 
-  
+: set-im12 ( instruction im12 -- instruction )
+  0b 111111111111 and		( instruction im12 )
+  0d 10 lshift                  ( instruction mask )
+  or
+;
+
+: set-im9 ( instruction im9 -- instruction )
+  0b 111111111 and		( instruction im9 )
+  0d 12 lshift                  ( instruction mask )
+  or
+;
+
+: set-shift ( instruction -- instruction )
+  0d 1 0d 22 lshift
+  or
+;
+
+ 
 ( These are the actual assembler instructions. )
 
-: ldrimmx ( rt rn -- instruction )
+: ldrimm ( rt rn -- instruction )
   0x f9400000			( push the basic opcode )
   swap set-rn			( rt ins )
   swap set-rt			( ins )
 ;
 
 : adr ( rt relative )
-  0x 10000000			( rt relative base-instruction )
+  0x 10000000			( rt relative opcode )
   swap set-im21			( rt instruction )
   swap set-rt
 ;
 
+: addimm ( rt rn imm9 -- instruction )
+  0x 91000000			( rt rn imm12 opcode )
+  swap set-im12			( rt rn instruction )
+  swap set-rn			( rn instuction )
+  swap set-rt			( instruction )
+;
+
+: subimm ( rt rn imm9 -- instruction )
+  0x d1000000			( rt rn imm12 opcode )
+  swap set-im12			( rt rn instruction )
+  swap set-rn			( rn instuction )
+  swap set-rt			( instruction )
+;
+
+
 (
   Very rough poc, this word will crash, but if you xray it you will
-  see the two instructions embedded inside of the definition.
+  see the instructions embedded inside of the definition.
 )
 
 : test1
   hello
   hello
   [
-  	17 17 ldrimmx w,
+  	1  10 addimm w,
+  	17 17 ldrimm w,
 	30 24 adr w,
   ]
   hello
